a8e116672b3799140d0f2860dd150b5c082f427c
xzha0379 <110176028+xzha0379@users.noreply.github.com>
2024-10-13 12:57:49 +1100

    Delete choropleth_map.js

--- choropleth_map.js
+++
@@ -1,4 +0,0 @@
-var map = "choropleth_map.json";
-vegaEmbed("#choropleth_map", map).then(function(result){
-    // Access vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
